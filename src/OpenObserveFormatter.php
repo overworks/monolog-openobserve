05366e5bb3db74dda95884611bad2ec8e6515198
Minhyung Park <urlinee@gmail.com>
2025-09-16 16:50:14 +0900

    Modify OpenObserveFormatter constructor

--- src/OpenObserveFormatter.php
+++ src/OpenObserveFormatter.php
@@ -7,6 +7,11 @@ use Monolog\LogRecord;
 
 class OpenObserveFormatter extends JsonFormatter
 {
+    public function __construct(int $batchMode = self::BATCH_MODE_JSON, bool $appendNewline = true, bool $ignoreEmptyContextAndExtra = true, bool $includeStacktraces = true)
+    {
+        parent::__construct($batchMode, $appendNewline, $ignoreEmptyContextAndExtra, $includeStacktraces);
+    }
+
     protected function normalizeRecord(LogRecord $record): array
     {
         $normalized = parent::normalizeRecord($record);
